18cb98c9ed6154a8111d2a93b9571f3f6af49914
ruairicaldwell <60053464+ruairica@users.noreply.github.com>
2023-07-03 18:44:23 +0100

    2021 day 3 csharp

---
+++ AdventOfCode/2021/csharp/Aoc2021/Aoc2021/Helpers.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aoc2021
+{
+    public static class Helpers
+    {
+        public static int ConvertToDecimal(List<int> binary)
+        {
+            var result = 0;
+            for (int m = 0; m < binary.Count; m++)
+            {
+                result += binary[m] * (int)Math.Pow(2, binary.Count - 1 - m);
+            }
+            return result;
+        }
+
+        public static void Deconstruct<T>(this IList<T> list, out T first)
+        {
+            first = list.Count > 0 ? list[0] : default;
+        }
+
+        public static void Deconstruct<T>(this IList<T> list, out T first, out T second)
+        {
+            first = list.Count > 0 ? list[0] : default;
+            second = list.Count > 1 ? list[1] : default;
+        }
+
+        public static void Deconstruct<T>(this IList<T> list, out T first, out T second, out T third)
+        {
+            first = list.Count > 0 ? list[0] : default;
+            second = list.Count > 1 ? list[1] : default;
+            third = list.Count > 2 ? list[2] : default;
+        }
+
+        public static void Deconstruct<T>(this IList<T> list, out T first, out T second, out T third, out T fourth)
+        {
+            first = list.Count > 0 ? list[0] : default;
+            second = list.Count > 1 ? list[1] : default;
+            third = list.Count > 2 ? list[2] : default;
+            fourth = list.Count > 3 ? list[3] : default;
+        }
+
+        public static void Deconstruct<T>(this IList<T> list, out T first, out T second, out T third, out T fourth, out T fifth)
+        {
+            first = list.Count > 0 ? list[0] : default;
+            second = list.Count > 1 ? list[1] : default;
+            third = list.Count > 2 ? list[2] : default;
+            fourth = list.Count > 3 ? list[3] : default;
+            fifth = list.Count > 4 ? list[4] : default;
+        }
+    }
+}
---
+++ AdventOfCode/2021/csharp/Aoc2021/Aoc2021/Tasks.cs
@@ -0,0 +1,136 @@
+﻿using Dumpify;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aoc2021;
+
+[TestFixture]
+public class Tasks
+{
+    private const string inputPath = @"C:\repos\AdventOfCode\AdventOfCode\2021\csharp\Aoc2021\Aoc2021\inputs\";
+
+    [Test]
+    public void day3_1()
+    {
+        var i = File.ReadAllText(inputPath + "/day3.txt")
+        .Trim()
+        .Split("\n")
+        .ToList();
+
+        var maxs = new List<int>();
+        var mins = new List<int>();
+        for (int x = 0; x < i[0].Length; x++)
+        {
+            var counter0 = 0;
+            var counter1 = 0;
+
+
+            for (int y = 0; y < i.Count; y++)
+            {
+                if (int.Parse(i[y][x].ToString()) == 1)
+                {
+                    counter1 += 1;
+                }
+                else
+                {
+                    counter0 += 1;
+                }
+            }
+            if (counter1 > counter0)
+            {
+                maxs.Add(1);
+                mins.Add(0);
+            }
+            else
+            {
+                mins.Add(1);
+                maxs.Add(0);
+            }
+        }
+
+        var mind = 0;
+        for (int m = 0; m < mins.Count; m++)
+        {
+            mind += mins[m] * (int)Math.Pow(2, mins.Count - 1 - m);
+        }
+
+        var maxd = 0;
+        for (int m = 0; m < maxs.Count; m++)
+        {
+            maxd += maxs[m] * (int)Math.Pow(2, maxs.Count - 1 - m);
+        }
+
+        Console.WriteLine(mind * maxd);
+    }
+
+    [Test]
+    public void day3_2()
+    {
+        var numbers = File.ReadAllText(inputPath + "/day3.txt")
+            .Trim()
+            .Split("\n")
+            .ToList();
+
+
+        // CO2 scrubber rating
+        var filteredMins = numbers.Select(x => x).ToList();
+
+        // oxygen generator rating
+        var filteredMaxes = numbers.Select(x => x).ToList();
+
+        var finalMax = "";
+        for (int i = 0; i < numbers[0].Length; i++)
+        {
+            var maxesDict = filteredMaxes
+            .GroupBy(x => x[i])
+            .ToDictionary(x => x.Key, x => x.ToList());
+
+            if (maxesDict.First().Value.Count == maxesDict.Last().Value.Count)
+            {
+                filteredMaxes = maxesDict['1'];
+            }
+            else
+            {
+                filteredMaxes = maxesDict
+                .MaxBy(x => x.Value.Count)
+                .Value;
+            }
+
+            if (filteredMaxes.Count == 1)
+            {
+                finalMax = filteredMaxes[0];
+                break;
+            }
+        }
+
+        var finalMin = "";
+        for (int i = 0; i < numbers[0].Length; i++)
+        {
+            var minsDict = filteredMins
+            .GroupBy(x => x[i])
+            .ToDictionary(x => x.Key, x => x.ToList());
+
+            if (minsDict.First().Value.Count == minsDict.Last().Value.Count)
+            {
+                filteredMins = minsDict['0'];
+            }
+            else
+            {
+                filteredMins = minsDict
+                .MinBy(x => x.Value.Count)
+                .Value;
+            }
+
+            if (filteredMins.Count == 1)
+            {
+                finalMin = filteredMins[0];
+                break;
+            }
+        }
+        Console.WriteLine(Helpers.ConvertToDecimal(finalMax.Select(x => int.Parse(x.ToString())).ToList()) * Helpers.ConvertToDecimal(finalMin.Select(x => int.Parse(x.ToString())).ToList()));
+    }
+}

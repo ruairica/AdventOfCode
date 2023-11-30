4b9b07bab2262460a19ab38c86be3b0598d88254
asos-ruairicaldwell <151652820+asos-ruairicaldwell@users.noreply.github.com>
2023-11-30 22:28:13 +0000

    set up for 2023

--- AdventOfCode/CSharp/Aoc/Aoc/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Dumpify;
-
-Console.WriteLine("Hello, World!");
-"TEST".Dump();
-
--- AdventOfCode/CSharp/Aoc/Aoc/Tasks2023.cs
+++ AdventOfCode/CSharp/Aoc/Aoc/Tasks2023.cs
@@ -7,19 +7,40 @@ namespace Aoc;
 [TestFixture]
 public class Tasks2023
 {
+    //run single test with eg dotnet watch test --filter day1_1_2023
     private const string basePath = "./inputs/2023";
 
     [Test]
     public void day1_1_2023()
     {
-        "2023 here".Dump();
-        FP.ReadFile($"{basePath}/day1.txt")
-            .Split("\n\n")
-            .Select(x => x.Split("\n").Select(int.Parse).Sum())
-            .MaxBy(x => x)
-            .Dump();
+        var result = 0;
+        var list = new List<int>();
+        var lines = FP.ReadFile($"{basePath}/day1.txt").Split("\n");
+
+        lines.Dump();
+
+        foreach (var (val, index) in lines.Enumerate())
+        {
+            // var num = int.Parse(val);
+        }
+
+        "day1 part 1".Dump();
     }
 
-    // Question: how do I run a dotnet test for the current file with nunit?
-    // Answer: dotnet test --filter "FullyQualifiedName~Tasks2023.day1_2_2023"
+    [Test]
+    public void day1_2_2023()
+    {
+        var result = 0;
+        var list = new List<int>();
+        var lines = FP.ReadFile($"{basePath}/day1.txt").Split("\n");
+
+        lines.Dump();
+
+        foreach (var (val, index) in lines.Enumerate())
+        {
+            // var num = int.Parse(val);
+        }
+
+        "day1 par 2".Dump();
+    }
 }

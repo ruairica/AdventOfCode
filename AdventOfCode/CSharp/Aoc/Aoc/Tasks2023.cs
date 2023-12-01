681ceda354efa637585f37eb8a12640f44d73a80
asos-ruairicaldwell <rcaldwell111@gmail.com>
2023-12-01 10:24:21 +0000

    fix git user + cleanup

--- AdventOfCode/CSharp/Aoc/Aoc/Tasks2023.cs
+++ AdventOfCode/CSharp/Aoc/Aoc/Tasks2023.cs
@@ -15,27 +15,29 @@ public class Tasks2023
     {
         var lines = FP.ReadFile($"{basePath}/day1.txt").Split("\n");
 
-        var result = lines.Select(line => line.Where(char.IsDigit).ToList()).Select(digits => int.Parse($"{digits[0]}{digits[^1]}")).Sum();
-
-        result.Dump();
+        lines
+            .Select(line => line.Where(char.IsDigit).ToList())
+            .Select(digits => int.Parse($"{digits[0]}{digits[^1]}"))
+            .Sum()
+            .Dump();
     }
 
     [Test]
     public void day1_2_2023()
     {
-        var numsStrings = new List<string>{ "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        var numStrings = new List<string>{ "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
         
         var dict = new Dictionary<string, int>();
-        numsStrings.Enumerate().ToList().ForEach(x=> dict.Add(x.val, x.index+1));
+        numStrings.Enumerate().ToList().ForEach(x=> dict.Add(x.val, x.index+1));
 
         var collection = Enumerable.Range(1, 9).Select(x => x.ToString()).ToList();
         collection.ForEach(x => dict.Add(x, int.Parse(x)));
 
-        numsStrings.AddRange(collection);
-        var lines = FP.ReadFile($"{basePath}/day1.txt").Split("\n");
+        numStrings.AddRange(collection);
 
-        var result = lines
-            .Select(line => numsStrings
+        var lines = FP.ReadFile($"{basePath}/day1.txt").Split("\n");
+        lines
+            .Select(line => numStrings
                 .SelectMany(x => line
                     .AllIndexOf(x)
                     .Select(y => (y, x))
@@ -43,15 +45,16 @@ public class Tasks2023
                     .ToList()))
             .Select(digits => 
                 int.Parse($"{dict[digits.MinBy(x => x.Item1).Item2]}{dict[digits.MaxBy(x => x.Item1).Item2]}"))
-            .Sum();
-
-        result.Dump();
+            .Sum()
+            .Dump();
     }
 
     [Test]
     public void day2_1_2023()
     {
-        var lines = FP.ReadFile($"{basePath}/day2.txt").Split("\n");
+        var lines = FP.ReadFile($"{basePath}/day2.txt")
+            .Split("\n");
+
         lines.Dump();
     }
 }

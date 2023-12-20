800c80e27fe8fb47fc1a9e9d0ffdfae32903e67f
asos-ruairicaldwell <60053464+ruairica@users.noreply.github.com>
2023-12-20 16:37:55 +0000

    move record into file

---
+++ AdventOfCode/CSharp/Aoc/Aoc/Utils/Condition.cs
@@ -0,0 +1,48 @@
+﻿namespace Aoc.Utils;
+
+
+public record Condition(string Rule)
+{
+    public bool hasCondition()
+    {
+        return Rule.Contains('<') || Rule.Contains('>');
+    }
+
+    public string? GetOutput(Dictionary<string, int> rating)
+    {
+        if (!hasCondition())
+        {
+            return Rule;
+        }
+
+        if (Rule.Contains('<'))
+        {
+            var parts = Rule.Split('<');
+            var left = parts[0];
+            var right = parts[1][..parts[1].IndexOf(':')];
+
+            if (rating[left] < int.Parse(right))
+            {
+                return Rule[(Rule.IndexOf(':') + 1)..];
+            }
+
+            return null;
+        }
+
+        if (Rule.Contains('>'))
+        {
+            var parts = Rule.Split('>');
+            var left = parts[0];
+            var right = parts[1][..parts[1].IndexOf(':')];
+
+            if (rating[left] > int.Parse(right))
+            {
+                return Rule[(Rule.IndexOf(':') + 1)..];
+            }
+
+            return null;
+        }
+
+        throw new Exception("Couldn't find rule");
+    }
+}

ad9644e556c678407c600c472f35920f7fa48d32
asos-ruairicaldwell <60053464+ruairica@users.noreply.github.com>
2024-12-13 17:37:33 +0000

    day 11 alt

--- AdventOfCode/CSharp/Aoc/Aoc2024/Day11.cs
+++ AdventOfCode/CSharp/Aoc/Aoc2024/Day11.cs
@@ -5,7 +5,11 @@ public class Day11 : Day
     public override void Part1() => RunForIterations(25);
 
 
-    public override void Part2() => RunForIterations(75);
+    public override void Part2()
+    {
+        RunForIterations(75);
+        AlternativeSolution();
+    }
 
     private void RunForIterations(int iterations)
     {
@@ -42,5 +46,48 @@ public class Day11 : Day
         }
 
         dict.Sum(x => x.Value).Dump();
+
+    }
+
+
+    private void AlternativeSolution()
+    {
+        Dictionary<(long, int), long> cache = new();
+
+        Text().Longs().Sum(x => Count(x, 2)).Dump();
+        // if we put stone though step, how long is the resulting list
+        long Count(long stone, int step)
+        {
+            long res;
+            if (step == 0)
+            {
+                return 1; // nothing to do, we just get element back so that length is 1
+            }
+
+            if (cache.TryGetValue((stone, step), out var v))
+            {
+                return v;
+            }
+
+            var s = stone.ToString();
+            if (stone == 0)
+            {
+                res = Count(1, step - 1);
+            }
+            else if (s.Length % 2 == 0)
+            {
+                var first = s[..(s.Length / 2)];
+                var second = s[(s.Length / 2)..];
+
+                res = Count(long.Parse(second), step - 1) + Count(long.Parse(first), step - 1);
+            }
+            else
+            {
+                res = Count(stone * 2024, step - 1);
+            }
+
+            cache.Add((stone, step), res);
+            return res;
+        }
     }
 }
